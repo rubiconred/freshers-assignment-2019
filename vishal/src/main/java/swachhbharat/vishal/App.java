b3725321d979629f094d07ee92e1eb6308297509
vishal <vishal@vishal-Vostro-3558>
2019-04-26 10:06:26 +0530

    Login with users in members.txt

--- vishal/src/main/java/swachhbharat/vishal/App.java
+++ vishal/src/main/java/swachhbharat/vishal/App.java
@@ -10,30 +10,31 @@ public class App
     public static void main( String[] args )
     {
     	Scanner sc = new Scanner(System.in);
-    	System.out.println("Login \nUsername & Password ");
+    	System.out.println("Login \nUsername, Password & Phonenumber");
     	String username=sc.nextLine();
     	String password=sc.nextLine();
+    	String phone=sc.nextLine();
     	String filepath="/home/vishal/git/freshers-assignment-2019/vishal/src/main/java/swachhbharat/vishal/members.txt";
     	sc.close();
     	
-    	if(verifyLogin(username,password,filepath))
+    	if(verifyLogin(username,password,phone,filepath))
     	{
-    	
-    			System.out.println("welcome \n Select items you wish to recycle\n");
-    			System.out.println("1.Plastic\n2.Glass\n3.Metal");
+    		System.out.println("Welcome"+username+ "\n Select items you wish to recycle\n");
+    		System.out.println("1.Plastic\n2.Glass\n3.Metal");
     		
     	}
     		
-    		else
-    			System.out.println("User not found");
+    	else
+    		System.out.println("User not found");
     	}
         
     
-    public static boolean verifyLogin(String username,String password,String filepath)
+    public static boolean verifyLogin(String username,String password,String phone,String filepath)
     {
     	boolean found=false;
     	String tempUsername="";
     	String tempPassword="";
+    	String tempPhone="";
     	try
     	{
     		x=new Scanner(new File(filepath));
@@ -43,24 +44,21 @@ public class App
     		{
     			tempUsername=x.next();
     			tempPassword=x.next();
+    			tempPhone=x.next();
     			
-    			if(tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim())) 
+    			if(tempUsername.trim().equals(username.trim()) && tempPassword.trim().equals(password.trim()) && tempPhone.trim().equals(phone.trim())) 
     			{
     				found=true;
-    				
     			}
     		
     		}
     		x.close();
     		
-    		
     	}
     	catch(Exception e)
     	{
     		System.out.println(e);
-    		
     	}
-    	
     return found;
     }
 }
